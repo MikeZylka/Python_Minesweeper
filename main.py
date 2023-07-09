9039c5b0a17f792107a1d893d2b175626d7e9e9d
MikeZylka <mikezylka123@live.com>
2023-07-09 19:07:37 -0400

    Initial cell class creation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from tkinter import *
 import sys
 sys.path.insert(1, './src')
+from tkinter import *
+from cell import Cell
 import settings
 import utils
+
 
 root = Tk()
 
@@ -35,6 +37,9 @@ center_frame = Frame(
     height=utils.height_prct(75)
 )
 center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
+
+c1 = Cell()
+
 
 # Run the Window
 root.mainloop()
