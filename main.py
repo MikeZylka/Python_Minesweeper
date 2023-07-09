7f7cdddc557a1cd6f780420f500ef0fb1538b560
MikeZylka <mikezylka123@live.com>
2023-07-09 16:31:00 -0400

    Initial window setup for the game

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+root.configure(bg="black")
+root.geometry('1440x720')
+root.title("Minesweeper")
+root.resizable(False, False)
+root.mainloop()
+
+root.mainloop()
