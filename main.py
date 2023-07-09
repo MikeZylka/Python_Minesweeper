a5a577922102ab8c62de351f02a485cf2d6e3c38
MikeZylka <mikezylka123@live.com>
2023-07-09 17:11:26 -0400

    creating a center fame and setting all frames to black

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ root.resizable(False, False)
 
 top_frame = Frame(
     root, 
-    bg='red', # Will be changed later
+    bg='black', # Will be changed later
     width=settings.WIDTH,
     height=utils.height_prct(25)
 )
@@ -22,11 +22,19 @@ top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
-    bg='blue',
+    bg='black',
     width=utils.width_prct(25),
     height=utils.height_prct(75)
 )
 left_frame.place(x=0, y=utils.height_prct(25))
+
+center_frame = Frame(
+    root, 
+    bg='black',
+    width=utils.width_prct(75),
+    height=utils.height_prct(75)
+)
+center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
 
 # Run the Window
 root.mainloop()
