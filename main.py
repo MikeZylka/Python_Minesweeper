2181b2a238388807ae30a1b356ee339b8c7181e0
MikeZylka <mikezylka123@live.com>
2023-07-09 17:08:32 -0400

    migrated hardcoded values from testing into utils and settings files

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from tkinter import *
+import sys
+sys.path.insert(1, './src')
+import settings
+import utils
 
 root = Tk()
 
 # Window Settings Override
 root.configure(bg="black")
-root.geometry('1440x720')
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title("Minesweeper")
 root.resizable(False, False)
 
 top_frame = Frame(
     root, 
     bg='red', # Will be changed later
-    width=1440,
-    height=180
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
 )
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
     bg='blue',
-    width=360,
-    height=540
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
 )
-left_frame.place(x=0, y=180)
+left_frame.place(x=0, y=utils.height_prct(25))
 
 # Run the Window
 root.mainloop()
