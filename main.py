a7b22624fe429999df5c3ad57411b873de6910e9
MikeZylka <mikezylka123@live.com>
2023-07-09 16:45:33 -0400

    initial frame placements and testing

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ from tkinter import *
 
 root = Tk()
 
+# Window Settings Override
 root.configure(bg="black")
 root.geometry('1440x720')
 root.title("Minesweeper")
 root.resizable(False, False)
-root.mainloop()
 
+top_frame = Frame(
+    root, 
+    bg='red', # Will be changed later
+    width=1440,
+    height=180
+)
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg='blue',
+    width=360,
+    height=540
+)
+left_frame.place(x=0, y=180)
+
+# Run the Window
 root.mainloop()
