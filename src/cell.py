339c4c6c36fc682e6d6215e977dac8ba18b1017e
MikeZylka <mikezylka123@live.com>
2023-07-11 13:16:05 -0400

    Completed left click functionality for cells; functionality for mines is partially complete, they only turn red and don't end the game

--- src/cell.py
+++ src/cell.py
@@ -21,19 +21,57 @@ class Cell:
             location,
             width=12,
             height=4,
-            text=f'{self.x}, {self.y}'
+            
         )
         btn.bind('<Button-1>', self.left_click_actions)
         btn.bind('<Button-3>', self.right_click_actions)
         self.cell_button_object = btn
 
     def left_click_actions(self, event):
-        print(event)
-        print("I am left clicked!")
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
 
     def right_click_actions(self, event):
         print(event)
         print("I am right clicked!")
+
+    def get_cell_by_axis(self, x,y):
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    @property
+    def surrounding_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x , self.y + 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+
+        ]
+        cells = [x for x in cells if x is not None]
+        return cells
+
+    @property
+    def surrounding_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounding_cells:
+            if cell.is_mine:
+                counter += 1
+        
+        return counter
+
+    def show_mine(self):
+        self.cell_button_object.configure(bg='red')
+
+    def show_cell(self):
+        self.cell_button_object.configure(text=self.surrounding_cells_mines_length)
 
     @staticmethod
     def randomize_mines():
